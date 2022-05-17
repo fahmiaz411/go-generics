e5f014087a3f15df8137b5c973e354541d25ad38
Fahmi Aziz <fahmiaziz884@gmail.com>
2022-05-17 15:25:45 +0700

    K, V, interface |

--- comparable_test.go
+++ comparable_test.go
@@ -10,7 +10,36 @@ func Comparable[T comparable](value1, value2 T) bool {
 	return value1 == value2
 }
 
+type Number interface{
+	int64 | float64
+}
+
+func SumNumbers[K comparable, V Number](m map[K]V) V {
+    var s V
+    for _, v := range m {
+        s += v
+    }
+    return s
+}
+
+func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+    var s V
+    for _, v := range m {
+        s += v
+    }
+    return s
+}
+
 func TestComparable(t *testing.T) {
 	assert.Equal(t, true, Comparable(100, 100))
 	assert.Equal(t, false, Comparable("eko", "fahmi"))
+
+	assert.Equal(t, int64(10), SumIntsOrFloats(map[string]int64{
+		"1":5,
+		"2":5,
+	}))
+	assert.Equal(t, 11.2, SumIntsOrFloats(map[string]float64{
+		"1": 5.6,
+		"2": 5.6,
+	}))
 }
